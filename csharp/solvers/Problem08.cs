416cfe15965f26b5fef39249d836c798e11d1b2b
Chad Nedzlek <chadnedz@microsoft.com>
2023-12-08 19:07:54 -0800

    Non cheaty, but much messier day 8 solution

--- csharp/solvers/Problem08.cs
+++ csharp/solvers/Problem08.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using ChadNedzlek.AdventOfCode.Library;
 
@@ -54,6 +55,92 @@ namespace ChadNedzlek.AdventOfCode.Y2023.CSharp.solvers
                     positions.Add(n);
             }
 
+            LameCheatyMethod(positions, inst, nodes);
+
+            CoolPerfectMethod(positions, inst, nodes);
+        }
+
+        private void CoolPerfectMethod(List<string> positions, string inst, Dictionary<string,(string left, string right)> nodes)
+        {
+            var solves = positions.Select(p => CalculateCycle(inst, nodes, p)).ToList();
+
+            var final = solves.Aggregate(CombineSolutions);
+            
+            Console.WriteLine($"Solution that isn't cheaty: {final.EndStates.Min() + final.Offset}");
+        }
+
+        private PotentialSolution CombineSolutions(PotentialSolution a, PotentialSolution b)
+        {
+            var newCycle = Helpers.Lcm(a.CycleLength, b.CycleLength);
+            var offset = Math.Max(a.Offset, b.Offset);
+
+            HashSet<long> newEnds = new();
+            foreach (var e in a.EndStates)
+            {
+                var newE = (e + a.Offset - offset + a.CycleLength) % a.CycleLength;
+                for (long i = 0; i < newCycle; i += a.CycleLength)
+                {
+                    var x = newE + i;
+                    if (b.EndStates.Contains((x - b.Offset) % b.CycleLength + offset))
+                    {
+                        newEnds.Add(x);
+                    }
+                }
+            }
+
+            var ret = new PotentialSolution($"{a.Name}+{b.Name}", offset, newCycle, newEnds.ToImmutableList());
+            Console.WriteLine(ret);
+            return ret;
+        }
+
+        private static PotentialSolution CalculateCycle(string inst, Dictionary<string, (string left, string right)> nodes, string pos)
+        {
+            Dictionary<(int index, string location), int> visited = new();
+            int i = 0;
+            var cur = pos;
+            List<int> endPoints = new List<int>();
+            while (true)
+            {
+                if (cur.EndsWith("Z"))
+                {
+                    endPoints.Add(i);
+                }
+
+                int modi = i % inst.Length;
+
+                if (visited.TryGetValue((modi, cur), out var when))
+                {
+                    var ret = new PotentialSolution(
+                        pos,
+                        when,
+                        i - when,
+                        endPoints.Select(e => (long)e - when).ToImmutableList()
+                    );
+                    Console.WriteLine(ret);
+                    return ret;
+                }
+
+                visited.Add((modi, cur), i);
+
+                i++;
+                cur = inst[modi] switch
+                {
+                    'L' => nodes[cur].left,
+                    'R' => nodes[cur].right,
+                };
+            }
+        }
+
+        public record PotentialSolution(string Name, long Offset, long CycleLength, ImmutableList<long> EndStates)
+        {
+            public override string ToString()
+            {
+                return $"{Name} : O={Offset}, C={CycleLength}, E={string.Join(", ", EndStates)}";
+            }
+        }
+
+        private static void LameCheatyMethod(List<string> positions, string inst, Dictionary<string, (string left, string right)> nodes)
+        {
             Dictionary<string, long> times = new();
             foreach (var p in positions)
             {
